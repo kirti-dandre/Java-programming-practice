ea2fa917e6b4e1c5cbba279e0a62af641332f07f
kirti-dandre <kirtidandre06@gmail.com>
2026-04-26 00:08:23 +0530

    Added If-Else Challenge

--- 3.1-ifElseChallenge/RangeNum15.java
+++ 3.1-ifElseChallenge/RangeNum15.java
@@ -5,9 +5,15 @@ public class RangeNum15 {
      Scanner input= new Scanner(System.in);
 
      // Check if a number lies within the range [100, 999]
-     System.out.print("Please enter any digit");
+     System.out.print("Please enter any digit: ");
+     int num = input.nextInt();
+     if(num >=100 && num <= 999) {
+      System.out.println(" Your Number is lies within the range");
+     }else{
+      System.out.println("Your number is not lied within the range");
+     }
 
-
+   input.close();
 
   }
   
