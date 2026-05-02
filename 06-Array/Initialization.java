0cf8cc8caf013d83563c78c0ac5f75c2d6c171b9
kirti-dandre <kirtidandre06@gmail.com>
2026-05-02 23:59:33 +0530

    Added Array

---
+++ 06-Array/Initialization.java
@@ -0,0 +1,15 @@
+public class Initialization {
+  public static void main(String[] args) {
+    int[] myArr = new int[4];
+    myArr[0] = 98;
+    myArr[0] = 6;
+    myArr[0] = 8;
+    myArr[0] = 9;
+    System.out.println(myArr[0]);
+    System.out.println(myArr[1]);
+    System.out.println(myArr[2]);
+    System.out.println(myArr[3]);
+     
+  }
+  
+}
