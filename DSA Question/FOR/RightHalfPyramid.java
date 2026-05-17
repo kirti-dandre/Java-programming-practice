8d7581f90d65007c0627e80bb9b960ebf05ac254
kirti-dandre <kirtidandre06@gmail.com>
2026-05-17 23:10:10 +0530

    Added DSA Question

---
+++ DSA Question/FOR/RightHalfPyramid.java
@@ -0,0 +1,22 @@
+package FOR;
+
+import java.util.Scanner;
+
+public class RightHalfPyramid {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter the number: ");
+    int number = input.nextInt();
+    for(int i = 1; i <= number; i++) {
+      for (int j = 1; j<= number-i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= i; j++)  {
+        System.out.print("*");
+      }
+      System.out.println();
+      input.close();
+    }
+  }
+  
+}
