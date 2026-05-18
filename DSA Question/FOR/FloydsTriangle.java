724964dbbbed298c82d5684cec6861d537d0e7d3
kirti-dandre <kirtidandre06@gmail.com>
2026-05-18 22:17:38 +0530

    Added Floyds Triangle in DSA Question

---
+++ DSA Question/FOR/FloydsTriangle.java
@@ -0,0 +1,21 @@
+package FOR;
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter the number: ");
+    int number = input.nextInt();
+    int num = 1;
+    for (int i = 1; i <= number; i++){
+      for (int j = 1; j <= i; j++) {
+        System.out.print(num +" ");
+        num++;
+      }
+      System.out.println();
+    }
+    input.close();
+  }
+  
+}
